64102b0b690cd18f705ee2d04ca3cb919d30ad0b
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-09-18 08:50:45 +0000

    feat(theme): add custom color tokens and text presets in @layer base

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { useState, useEffect, useRef } from "react";
 function App() {
   return (
     <>
+      <div className="dark">
+        <p className="text-[100px] bg-[var(--color-yellow-500)] dark:bg-[var(--color-dark-900)] dark:text-amber-50">
+          Hello World
+        </p>
+      </div>
       <h1>Hi My App</h1>
     </>
   );
